b1eae43e4256d5ea2d90e879c42d2f85276cb675
nozawama <6319519@ed.tus.ac.jp>
2021-03-25 18:21:59 +0900

    Update user registration function and post function

--- app/src/main/kotlin/com/task/sns/sns/app/request/PostRequest.kt
+++ app/src/main/kotlin/com/task/sns/sns/app/request/PostRequest.kt
@@ -1,10 +1,12 @@
 package com.task.sns.sns.app.request
 
+import com.task.sns.sns.domain.entity.User
 import java.util.*
 
 data class PostRequest (
-    var postId : Int = 0,
-    var userId : Int = 0,
-    var comment : String = "",
-    var createAt : Date = Date()
+    var postId: Int = 0,
+    var userId: Int = 0,
+    var comment: String = "",
+    var createAt: Date = Date(),
+    var user: User
 )
