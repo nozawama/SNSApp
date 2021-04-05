d523c9ca1ab5f55160b3aa94744c6303f2f4f9d6
nozawama <6319519@ed.tus.ac.jp>
2021-04-05 18:21:31 +0900

    Update database init file

--- docker-compose/db/mysql_init/create-table.sql
+++ docker-compose/db/mysql_init/create-table.sql
@@ -1,15 +1,17 @@
+CREATE database app_db IF NOT EXISTS `app_db`;
+
 USE app_db;
 
-DROP TABLE IF EXISTS `Like`;
+-- DROP TABLE IF EXISTS `Like`;
 
-CREATE TABLE `Like`
-    (user_id INTEGER UNSIGNED,
-    post_id INTEGER UNSIGNED
-    );
+-- CREATE TABLE `Like`
+--     (user_id INTEGER UNSIGNED,
+--     post_id INTEGER UNSIGNED
+--     );
 
-INSERT
-    INTO `Like`(user_id, post_id)
-    VALUES 
-        (1, 2),
-        (2, 3),
-        (1, 3);
+-- INSERT
+--     INTO `Like`(user_id, post_id)
+--     VALUES 
+--         (1, 2),
+--         (2, 3),
+--         (1, 3);
