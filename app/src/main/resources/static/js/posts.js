9e3346335b9ebf7f27adc60a164b690bcef16a5a
nozawama <6319519@ed.tus.ac.jp>
2021-04-06 13:06:11 +0900

    Update view

--- app/src/main/resources/static/js/posts.js
+++ app/src/main/resources/static/js/posts.js
@@ -4,9 +4,9 @@ window.onload = function() {
     $('.areaPost:nth-child(n + ' + (moreNum + 1) + ')').addClass('is_hidden');
 }
 
-$(document).on('click', '.buttonMore button', function() {
+$(document).on('click', '.buttonMore',　function() {
     $('.areaPost.is_hidden').slice(0, moreNum).removeClass('is_hidden');
     if ($('.areaPost.is_hidden').length == 0) {
-        $('.buttonMore button').remove();
+        $('.buttonMore').remove();
     }
 });
