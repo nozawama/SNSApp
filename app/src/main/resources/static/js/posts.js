f749da5f7e360a1ea9dbfc2668110998d3e68adc
nozawama <6319519@ed.tus.ac.jp>
2021-04-07 16:50:55 +0900

    Change behavior of logout button

--- app/src/main/resources/static/js/posts.js
+++ app/src/main/resources/static/js/posts.js
@@ -2,8 +2,11 @@ var moreNum = 5;
 
 window.onload = function() {
     var list_post = document.getElementsByClassName("areaPost");
-    for(var i = moreNum; i < list.length; i++) {
+    for(var i = moreNum; i < list_post.length; i++) {
         list_post.item(i).classList.add("is_hidden");
+    }
+    if (list_post.length <= moreNum) {
+        document.getElementById("buttonMore").remove();
     }
 }
 
@@ -12,7 +15,7 @@ document.getElementById("buttonMore").onclick = function () {
     for(var i = 0; i < moreNum; i++) {
         if(list_hidden.length > 0) list_hidden.item(0).classList.remove("is_hidden");
         else break;
-    }
+    
     if (list_hidden.length == 0) {
         document.getElementById("buttonMore").remove();
     }
