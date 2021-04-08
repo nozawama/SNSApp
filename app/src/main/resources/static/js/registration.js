263061c332385194fa36a78e7d9db7e5f8b437e6
nozawama <6319519@ed.tus.ac.jp>
2021-04-08 12:40:52 +0900

    パスワード不一致後再入力時の挙動を修正

--- app/src/main/resources/static/js/registration.js
+++ app/src/main/resources/static/js/registration.js
@@ -1,8 +1,9 @@
 var form = document.getElementById("formRegister");
 
 document.getElementById("buttonSubmit").onclick = function () {
-    form.password.setCustomValidity("");
     if(form.password.value != form.passwordConfirm.value) {
         form.passwordConfirm.setCustomValidity("パスワードが一致しません。");
+    } else {
+        form.passwordConfirm.setCustomValidity("");
     }
 }
