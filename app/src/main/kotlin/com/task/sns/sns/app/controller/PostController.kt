311b9a1213f2a7f5d22f29254a9913880e278f1b
nozawama <6319519@ed.tus.ac.jp>
2021-04-14 15:06:01 +0900

    ローカル変数postをvar->valに変更, 不要な改行の削除

--- app/src/main/kotlin/com/task/sns/sns/app/controller/PostController.kt
+++ app/src/main/kotlin/com/task/sns/sns/app/controller/PostController.kt
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.*;
-import javax.transaction.Transactional
+import org.springframework.web.bind.annotation.*
 
 @Controller
 class PostController {
-
     @Autowired
     lateinit var postRepository: PostRepository
 
@@ -33,7 +31,6 @@ class PostController {
 
     @GetMapping("/post")
     fun getPostList(@AuthenticationPrincipal userDetailsImpl: UserDetailsImpl, model: Model) : String {
-
         model.addAttribute("posts", postRepository.findAll().sortedByDescending { it.createAt })
 
         model.addAttribute("user", userDetailsImpl.user)
@@ -43,11 +40,11 @@ class PostController {
 
     @PostMapping("/post/delete")
     fun deletePost(@AuthenticationPrincipal userDetailsImpl: UserDetailsImpl, @ModelAttribute postRequest: PostRequest): String {
-        var post = postRepository.findById(postRequest.postId)
+        val post = postRepository.findById(postRequest.postId)
         if(post.isPresent && post.get().user.userId == userDetailsImpl.user.userId) {
             postRepository.deleteById(postRequest.postId)
         }
+
         return "redirect:/post"
     }
-
 }
